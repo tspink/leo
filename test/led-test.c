76ff1879df36e2ac0c2a838ba73dd8401b0e86d2
Tom Spink <tspink@gmail.com>
2014-12-17 12:45:19 +0000

    Maybe turn the LED on

--- test/led-test.c
+++ test/led-test.c
@@ -2,5 +2,14 @@
 
 void app_start()
 {
+	unsigned char *portf_base = (unsigned char *)0x40025000;
+	unsigned int *portf_gpiodata = (unsigned int *)portf_base;
+	unsigned int *portf_gpiodir = (unsigned int *)(portf_base + 0x400);
+	unsigned int *rcgcgpio = (unsigned int *)0x400fe608;
+
+	*rcgcgpio |= 32;
+	*portf_gpiodir |= 2;
+	*portf_gpiodata |= 2;
+
 	for (;;);
 }
