dbd9b58486608b00b6b1376864d0838d0f441634
Tom Spink <tspink@gmail.com>
2014-12-17 13:16:30 +0000

    Use the AHB for GPIO

--- test/led-test.c
+++ test/led-test.c
@@ -2,12 +2,22 @@
 
 void app_start()
 {
-	unsigned char *portf_base = (unsigned char *)0x40025000;
+	unsigned char *portf_base = (unsigned char *)0x4005d000;
 	unsigned int *portf_gpiodata = (unsigned int *)portf_base;
 	unsigned int *portf_gpiodir = (unsigned int *)(portf_base + 0x400);
+	unsigned int *portf_gpioden = (unsigned int *)(portf_base + 0x51c);
+	unsigned int *gpiohbctl = (unsigned int *)0x400fe06c;
 	unsigned int *rcgcgpio = (unsigned int *)0x400fe608;
 
-	*rcgcgpio |= 32;
+	*gpiohbctl |= 1 << 5;
+	*rcgcgpio |= 1 << 5;
+	
+	asm volatile("nop\n");
+	asm volatile("nop\n");
+	asm volatile("nop\n");
+	asm volatile("nop\n");
+	
+	*portf_gpioden |= 2;
 	*portf_gpiodir |= 2;
 	*portf_gpiodata |= 2;
 
